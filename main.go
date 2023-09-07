4bca7b664d4a43304d66247dfd6161f60eceabe3
Nikhil George <nikhilgeorge2015@gmail.com>
2023-09-07 14:42:37 +0530

    add: generate update + delete statements with tests

--- main.go
+++ main.go
@@ -15,14 +15,28 @@ type OplogEntry struct {
 	Op string                 `json:"op"`
 	NS string                 `json:"ns"`
 	O  map[string]interface{} `json:"o"`
+	O2 map[string]interface{} `json:"o2"`
 }
 
-func GenerateInsertSQL(oplog string) (string, error) {
+func GenerateSQL(oplog string) (string, error) {
 	var oplogObj OplogEntry
 	if err := json.Unmarshal([]byte(oplog), &oplogObj); err != nil {
 		return "", err
 	}
 
+	switch oplogObj.Op {
+	case "i":
+		return generateInsertSQL(oplogObj)
+	case "u":
+		return generateUpdateSQL(oplogObj)
+	case "d":
+		return generateDeleteSQL(oplogObj)
+	}
+
+	return "", fmt.Errorf("invalid oplog")
+}
+
+func generateInsertSQL(oplogObj OplogEntry) (string, error) {
 	switch oplogObj.Op {
 	case "i":
 		sql := fmt.Sprintf("INSERT INTO %s", oplogObj.NS)
@@ -44,7 +58,72 @@ func GenerateInsertSQL(oplog string) (string, error) {
 		return sql, nil
 	}
 
-	return "", nil
+	return "", fmt.Errorf("invalid oplog")
+}
+
+func generateUpdateSQL(oplogObj OplogEntry) (string, error) {
+	switch oplogObj.Op {
+	case "u":
+		sql := fmt.Sprintf("UPDATE %s SET", oplogObj.NS)
+
+		diffMap, ok := oplogObj.O["diff"].(map[string]interface{})
+		if !ok {
+			return "", fmt.Errorf("invalid oplog")
+		}
+
+		if setMap, ok := diffMap["u"].(map[string]interface{}); ok {
+			columnValues := make([]string, 0, len(setMap))
+			for columnName, value := range setMap {
+				columnValues = append(columnValues, fmt.Sprintf("%s = %s", columnName, getColumnValue(value)))
+			}
+
+			sort.Strings(columnValues)
+
+			sql = fmt.Sprintf("%s %s", sql, strings.Join(columnValues, ", "))
+		} else if unsetMap, ok := diffMap["d"].(map[string]interface{}); ok {
+			columnValues := make([]string, 0, len(unsetMap))
+			for columnName := range unsetMap {
+				columnValues = append(columnValues, fmt.Sprintf("%s = NULL", columnName))
+			}
+
+			sort.Strings(columnValues)
+
+			sql = fmt.Sprintf("%s %s", sql, strings.Join(columnValues, ", "))
+		} else {
+			return "", fmt.Errorf("invalid oplog")
+		}
+
+		whereClauseCols := make([]string, 0, len(oplogObj.O2))
+		for columnName, value := range oplogObj.O2 {
+			whereClauseCols = append(whereClauseCols, fmt.Sprintf("%s = %s", columnName, getColumnValue(value)))
+		}
+
+		sql = fmt.Sprintf("%s WHERE %s;", sql, strings.Join(whereClauseCols, ", "))
+
+		return sql, nil
+	}
+
+	return "", fmt.Errorf("invalid oplog")
+}
+
+func generateDeleteSQL(oplogObj OplogEntry) (string, error) {
+	switch oplogObj.Op {
+	case "d":
+		sql := fmt.Sprintf("DELETE FROM %s WHERE", oplogObj.NS)
+
+		whereColumnValues := make([]string, 0, len(oplogObj.O))
+		for columnName, value := range oplogObj.O {
+			whereColumnValues = append(whereColumnValues, fmt.Sprintf("%s = %s", columnName, getColumnValue(value)))
+		}
+
+		sort.Strings(whereColumnValues)
+
+		sql = fmt.Sprintf("%s %s;", sql, strings.Join(whereColumnValues, " AND "))
+
+		return sql, nil
+	}
+
+	return "", fmt.Errorf("invalid oplog")
 }
 
 func getColumnValue(value interface{}) string {
